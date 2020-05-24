a7dd5fbbb426c7a0b8be2255ad6b19c5d07178bc
Anna Malorodova <angervenus6@gmail.com>
2020-05-24 21:08:20 +0300

    About us, our works and our promos sections added

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 document.querySelector('.menu-icon-wrapper').onclick = function(){
     document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
 }
+
+
+
+
+/*const person1 = {
+    name: 'Anna',
+    surname: 'Malorodova',
+
+    sayHello () {
+        console.log(`Hello, ${this.name} ${this.surname}`)
+    }
+}
+
+const person2 = {
+    name: 'Aida',
+    surname: 'Mordorova',
+
+    sayHello () {
+        console.log(`Hello, ${this.name} ${this.surname}`)
+    }
+}
+
+
+person1.sayHello()
+person2.sayHello()
+
+function getPerson (name, surname, age){
+const person = {
+ name: name,
+ surname: surname,
+ age:age,
+
+ sayHello () {
+    console.log(`Hello, ${this.name} ${this.surname}`)
+}
+}
+
+return person
+
+}
+
+const person1 = getPerson('Anna', 'Malorodova')
+const person2 = getPerson('Aida', 'Mordorova')
+person1.sayHello()
+person2.sayHello()*/
