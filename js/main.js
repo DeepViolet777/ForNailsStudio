6f77286a6da9442df1ea8ca004c54e2922523db8
Anna Malorodova <angervenus6@gmail.com>
2020-09-07 21:32:47 +0300

    Block pricing

--- js/main.js
+++ js/main.js
@@ -1,72 +1,32 @@
 $(document).ready(function(){
+
   $('.slider-container').slick({
        dots:true
 
   });
 
   document.querySelector('.menu-icon-wrapper').onclick = function(){
-    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-}
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active')};
 
 
+let wraps = document.querySelectorAll(".toggle-btn_wrapper");
+
+wraps.forEach((btn)=>{
+   btn.addEventListener('click', function(){
+      toggleBtns = btn.querySelector(".toggle-btn")
+      toggleBtns.classList.toggle("toggle-btn-active")
+    
+     let sub = btn.parentNode.querySelector(".prices-sublist")
+      if (sub){
+         sub.classList.toggle("hidden");
+         
+      
+    
+    };
+});
+});
 });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const person1 = {
-    name: 'Anna',
-    surname: 'Malorodova',
-
-    sayHello () {
-        console.log(`Hello, ${this.name} ${this.surname}`)
-    }
-}
-
-const person2 = {
-    name: 'Aida',
-    surname: 'Mordorova',
-
-    sayHello () {
-        console.log(`Hello, ${this.name} ${this.surname}`)
-    }
-}
-
-
-person1.sayHello()
-person2.sayHello()
-
-function getPerson (name, surname, age){
-const person = {
- name: name,
- surname: surname,
- age:age,
-
- sayHello () {
-    console.log(`Hello, ${this.name} ${this.surname}`)
-}
-}
-
-return person
-
-}
-
-const person1 = getPerson('Anna', 'Malorodova')
-const person2 = getPerson('Aida', 'Mordorova')
-person1.sayHello()
-person2.sayHello()*/
